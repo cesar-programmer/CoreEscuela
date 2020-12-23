3b8dbc0f78b851762b93e78f4c32c7fa95755e06
cesar cordova <cesarcordova.developer@gmail.com>
2020-12-22 18:54:28 -0800

    agregamos el array

--- Program.cs
+++ Program.cs
@@ -10,28 +10,41 @@ namespace Etapa1
             var escuela = new Escuela("juan escutia", 2002, TiposEscuela.Primaria, ciudad:"tijuana", pais:"mexico");
             
 
-            var curso1 = new Curso()
-            {
-                Nombre="101"
-            };
+            var arregloCursos = new Curso[3];
+
+            arregloCursos[0] = new Curso()
+                                {
+                                    Nombre="101"
+                                };
 
             var curso2 = new Curso()
             {
                 Nombre="201"
             };
+            arregloCursos[1] = curso2;
 
-            var curso3 = new Curso()
-            {
-                Nombre="301"
-            };
 
+            arregloCursos[2] = new Curso
+                                {
+                                    Nombre="301"
+                                };
+// estas son diferentes formas de asignar el objeto al arrray 
             Console.WriteLine(escuela);
-           
             System.Console.WriteLine("==========================");
-            Console.WriteLine(curso1.Nombre +","+ curso1.UniqueId);
-            Console.WriteLine($"{curso2.Nombre} , {curso2.UniqueId}");
-            Console.WriteLine(curso3);
+            ImprimirCursos(arregloCursos);
+// para crear metodos de forma efectiva solo escribe la linea del metodo y pulsa control punto y generar metodo
+        }
 
+        private static void ImprimirCursos(Curso[] arregloCursos)
+        {
+            int counter =0;
+           while (counter < arregloCursos.Length)
+           {
+               Console.WriteLine($"Nombre {arregloCursos[counter].Nombre}, id {arregloCursos[counter].UniqueId}");
+               // esta es una manera mas bonita de imprimir datos de un array
+               counter += 1;
+               //con esta manera de aumentar el contador puedes aumentar el numero como en vez de pober el 1 puedes poner el 2 o el 3 e ira sumando de 2 en 2 o de 3 en 3
+           }
         }
     }
 }
